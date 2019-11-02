92450a7ec2031d524e046f01cc121d94bb08b0c8
YonghoChoi <yongho1037@gmail.com>
2019-11-02 16:52:31 +0900

    firebase SDK 사용하도록 FCM 패키지 내용 수정

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module github.com/YonghoChoi/depromeet-soboon
 go 1.12
 
 require (
-	github.com/appleboy/go-fcm v0.1.4
+	cloud.google.com/go/firestore v1.0.0 // indirect
+	cloud.google.com/go/storage v1.1.2 // indirect
+	firebase.google.com/go v3.10.0+incompatible
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20191029031824-8986dd9e96cf // indirect
+	google.golang.org/api v0.13.0
+	google.golang.org/appengine v1.6.5 // indirect
 )
--- pkg/fcm/fcm_test.go
+++ pkg/fcm/fcm_test.go
@@ -1,32 +1,46 @@
 package fcm
 
 import (
+	"context"
+	"firebase.google.com/go"
+	"firebase.google.com/go/messaging"
+	"fmt"
+	"google.golang.org/api/option"
 	"log"
 	"testing"
 )
 
-// ref : github.com/appleboy/go-fcm
 func TestFCMSend(t *testing.T) {
-	// Create the message to be sent.
-	msg := &Message{
-		To: "sample_device_token",
-		Data: map[string]interface{}{
-			"foo": "bar",
+	ctx := context.Background()
+	app, err := firebase.NewApp(ctx, nil, option.WithCredentialsFile("../../private/soboon-service-account.json"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client, err := app.Messaging(ctx)
+	if err != nil {
+		log.Fatalf("error getting Messaging client: %v\n", err)
+	}
+
+	// 간단한 테스트를 위해 클라이언트로 https://github.com/firebase/quickstart-js/tree/master/messaging 사용
+	registrationToken := "<클라이언트의 token 입력>"
+
+	// See documentation on defining a message payload.
+	message := &messaging.Message{
+		Data: map[string]string{
+			"score": "850",
+			"time":  "2:45",
 		},
+		Token: registrationToken,
 	}
 
-	// Create a FCM client to send the message.
-	client, err := NewClient("sample_api_key")
+	// Send a message to the device corresponding to the provided
+	// registration token.
+	response, err := client.Send(ctx, message)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	// Send the message and receive the response without retries.
-	response, err := client.Send(msg)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Printf("%#v\n", response)
+	// Response is a message ID string.
+	fmt.Println("Successfully sent message:", response)
 
 }
